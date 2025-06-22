180db971d611fa215be2c1e11288988363d9ca8d
Shreyas M <137000011+shreyasm-codes@users.noreply.github.com>
2025-06-22 13:42:39 +0530

    Update TestController.java
    Added one more method for controller

--- src/main/java/demo/projects/issues_tracker_backend/controller/TestController.java
+++ src/main/java/demo/projects/issues_tracker_backend/controller/TestController.java
@@ -1,14 +1,21 @@
 package demo.projects.issues_tracker_backend.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class TestController {
 
     @GetMapping("/api/hello")
     public String hello(@RequestParam String input) {
         return String.format("Hello from backend! You have selected: %s  ", input);
     }
+
+     @GetMapping("/api/test")
+     public String test() {
+        return "This is test message..";
+     }
 }
