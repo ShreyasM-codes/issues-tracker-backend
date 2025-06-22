2863296ee9bc6d7ba002bc9b185506ed4bb54e62
ShreyasM-codes <shreyasravi12350@gmail.com>
2025-06-22 13:13:49 +0530

    Update TestController to accept input parameter in hello method

--- src/main/java/demo/projects/issues_tracker_backend/controller/TestController.java
+++ src/main/java/demo/projects/issues_tracker_backend/controller/TestController.java
@@ -1,13 +1,14 @@
 package demo.projects.issues_tracker_backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
     @GetMapping("/api/hello")
-    public String hello() {
-        return "hello from backend!";
+    public String hello(@RequestParam String input) {
+        return String.format("Hello from backend! You have selected: %s  ", input);
     }
 }
