7abf82f196784fc80f16c43a90ef5c28f5108275
Shreyas M <137000011+ShreyasM-codes@users.noreply.github.com>
2025-06-22 16:25:32 +0530

    Update TestController.java

--- src/main/java/demo/projects/issues_tracker_backend/controller/TestController.java
+++ src/main/java/demo/projects/issues_tracker_backend/controller/TestController.java
@@ -1,10 +1,12 @@
 package demo.projects.issues_tracker_backend.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class TestController {
 
     @GetMapping("/api/hello")
@@ -13,7 +15,7 @@ public class TestController {
     }
 
      @GetMapping("/api/test")
-    public String test() {
+     public String test() {
         return "This is test message..";
-    }
+     }
 }
